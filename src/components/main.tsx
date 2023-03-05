6af9a2867d24bdc3506578c61b1530d12caa2e8a
lHarlequinl <alyoshin4@mail.ru>
2023-03-05 13:23:12 +0300

    commit

---
+++ src/components/main.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+
+const Main = (): ReactElement<HTMLDivElement> => {
+    return (
+        <main className="main">
+        </main>
+    );
+};
+
+export default Main;
