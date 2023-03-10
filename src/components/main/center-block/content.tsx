c7e5ae35d274fde3716080c6393d49b47e688e3a
lHarlequinl <alyoshin4@mail.ru>
2023-03-10 21:06:17 +0300

    add playlist-title

---
+++ src/components/main/center-block/content.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import PlaylistTitle from './playlist/playlist-title';
+
+function Content() {
+	return (
+        <div className="centerblock__content">
+            <PlaylistTitle />
+        </div>
+	)
+}
+
+export default Content
