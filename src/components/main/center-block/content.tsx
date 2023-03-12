26747b8b150ea4bd659b8de1f8c21bd2ef337a73
lHarlequinl <alyoshin4@mail.ru>
2023-03-12 14:31:11 +0300

    add playlist items

--- src/components/main/center-block/content.tsx
+++ src/components/main/center-block/content.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
 import PlaylistTitle from './playlist/playlist-title';
+import PlaylistItems from './playlist/playlist-items';
 
-function Content() {
+function Content(): ReactElement<HTMLDivElement> {
 	return (
         <div className="centerblock__content">
             <PlaylistTitle />
+            <PlaylistItems />
         </div>
 	)
 }
