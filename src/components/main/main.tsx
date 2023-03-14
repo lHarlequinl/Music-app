91006510b6581722046546f2184621a70d043457
lHarlequinl <alyoshin4@mail.ru>
2023-03-14 19:41:45 +0300

    add new blocks

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,14 +1,16 @@
 import React, { ReactElement } from 'react';
-import Navigation from './main/navigation/navigation';
-import CentrBlock from './main/center-block/center-block';
-// import Sidebar from './main/sidebar/sidebar';
+import Navigation from './navigation/navigation';
+import CentrBlock from './center-block/center-block';
+import Bar from '../bar/bar';
+import Sidebar from './sidebar/sidebar';
 
 const Main = (): ReactElement<HTMLDivElement> => {
     return (
         <main className="main">
             <Navigation />
             <CentrBlock />
-            {/* <Sidebar /> */}
+            <Sidebar />
+            <Bar />
         </main>
     );
 };
--- src/components/main/sidebar/sidebar.tsx
+++ src/components/main/sidebar/sidebar.tsx
@@ -1,16 +1,16 @@
-import React, { ReactElement } from 'react';
+import React, { FC } from 'react';
+import SidebarUser from './sidebar-user';
+import SidebarList from './sidebar-list';
 
-interface ImageProps {
-    src: string;
-    alt: string;
-}
-
-function Sidebar(): ReactElement<HTMLDivElement> {
+const Sidebar: FC = () => {
     return (
-		<div>
-
-		</div>
-	);
-}
+        <div className="main__sidebar sidebar">
+            <SidebarUser name="Maksim.Aleshin" />
+            <div className="sidebar__block">
+                <SidebarList />
+            </div>
+        </div>
+    );
+};
 
 export default Sidebar;
