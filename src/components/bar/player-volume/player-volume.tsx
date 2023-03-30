92426a5e03841e874ce7783cd4b913d8c9884fc2
lHarlequinl <alyoshin4@mail.ru>
2023-03-30 20:49:51 +0300

    add volume-change and timer-progress

--- src/components/bar/player-volume/player-volume.tsx
+++ src/components/bar/player-volume/player-volume.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import SvgImage from '../../svg/svg-image';
-import * as S from './player-volume.style';
+import * as Styled from './player-volume.style';
+interface Props {
+    value: number;
+    onVolumeChange: (e: React.ChangeEvent) => void;
+    onVolumeToggle: () => void;
+    onVolumeOn: boolean;
+}
 
-const PlayerVolume = () => {
+const PlayerVolume = ({
+    value,
+    onVolumeChange,
+    onVolumeToggle,
+    onVolumeOn = true,
+}: Props) => {
     return (
-        <S.PlayerVolumeBlock>
-            <S.PlayerVolumeContent>
-                <S.PlayerVolumeImageWrapper>
-                    <S.PlayerVolumeSVGWrapper>
+        <Styled.PlayerVolumeBlock>
+            <Styled.PlayerVolumeContent>
+                <Styled.PlayerVolumeImageWrapper>
+                    <Styled.PlayerVolumeSVGWrapper
+                        onClick={() => onVolumeToggle()}
+                    >
                         <SvgImage
-                            href="/img/icon/sprite.svg#icon-volume"
+                            href={
+                                onVolumeOn
+                                    ? '/img/icon/sprite.svg#icon-volume'
+                                    : '/img/icon/sprite.svg#icon-volume-off'
+                            }
                             ariaLabel="volume"
                         />
-                    </S.PlayerVolumeSVGWrapper>
-                </S.PlayerVolumeImageWrapper>
-                <S.PlayerVolumeProgressWrapper>
-                    <S.PlayerVolumeProgressLine type="range" name="range" />
-                </S.PlayerVolumeProgressWrapper>
-            </S.PlayerVolumeContent>
-        </S.PlayerVolumeBlock>
+                    </Styled.PlayerVolumeSVGWrapper>
+                </Styled.PlayerVolumeImageWrapper>
+                <Styled.PlayerVolumeProgressWrapper>
+                    <Styled.PlayerVolumeProgressLine
+                        type="range"
+                        name="volume"
+                        min='0'
+                        max='1'
+                        step='0.01'
+                        value={value}
+                        onChange={(e) => onVolumeChange(e)}
+                    />
+                </Styled.PlayerVolumeProgressWrapper>
+            </Styled.PlayerVolumeContent>
+        </Styled.PlayerVolumeBlock>
     );
 };
 
--- src/components/bar/player/player.tsx
+++ src/components/bar/player/player.tsx
@@ -1,29 +1,104 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import * as Styled from './player.style';
 import PlayerControls from '../player-controls/player-controls';
 import PlayerTrack from '../player-track/player-track';
+import PlayerVolume from '../player-volume/player-volume';
 
 const Player = () => {
-    const [status, setStatus] = useState<boolean>(true);
+    const [isLoading, setStatus] = useState<boolean>(true);
+
     useEffect(() => {
         const loadTimer = setTimeout(() => {
             setStatus(false);
-        }, 5000);
+        }, 0);
         return () => {
             clearTimeout(loadTimer);
         };
     });
+
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [trackProgress, setTrackProgress] = useState<number>(0);
+    const [isVolumeOn, setIsVolumeOn] = useState<boolean>(true);
+
+    const audioRef = useRef(new Audio('/music/dropin.mp3'));
+    const interval: any = useRef();
+
+    const { duration } = audioRef.current;
+    const progressByPercent = (trackProgress / duration) * 100;
+
+    const onTogglePlay = () => {
+        setIsPlaying(!isPlaying);
+    };
+
+    const onVolumeChange = (e: React.ChangeEvent) => {
+        audioRef.current.volume = Number((e.target as HTMLInputElement).value);
+        setIsVolumeOn(true);
+    };
+
+    const onVolumeToggle = () => {
+        setIsVolumeOn(!isVolumeOn);
+        isVolumeOn
+            ? (audioRef.current.volume = 0)
+            : (audioRef.current.volume = 0.5);
+    };
+
+    const startProgressTimer = (): void => {
+        clearInterval(interval.current);
+
+        interval.current = setInterval(() => {
+            setTrackProgress(audioRef.current.currentTime);
+
+            if (audioRef.current.ended) {
+                setIsPlaying(false);
+            }
+        }, 1000);
+    };
+
+    useEffect(() => {
+        if (isPlaying) {
+            audioRef.current.play().catch((e) => {
+                console.log(e);
+            });
+            startProgressTimer();
+        } else {
+            audioRef.current.pause();
+        }
+    }, [isPlaying]);
+
     return (
-        <Styled.BarPlayer>
-            <PlayerControls />
-            <PlayerTrack
-                isLoading={status}
-                trackLink="https://"
-                trackName="Ты та..."
-                authorLink="https://"
-                authorName="Баста"
-            />
-        </Styled.BarPlayer>
+        <Styled.BarPlayerContent>
+            <Styled.BarPlayerProgress
+                type="range"
+                step="1"
+                min="0"
+                max={isNaN(duration) ? '100' : `${duration}`}
+                value={trackProgress}
+                gradientValue={
+                    isNaN(progressByPercent) ? '0' : `${progressByPercent}`
+                }
+            ></Styled.BarPlayerProgress>
+            <Styled.BarPlayerBlock>
+                <Styled.BarPlayer>
+                    <PlayerControls
+                        isPlaying={isPlaying}
+                        onTogglePlay={() => onTogglePlay()}
+                    />
+                    <PlayerTrack
+                        isLoading={isLoading}
+                        trackLink="https://"
+                        trackName="Dropin"
+                        authorLink="https://"
+                        authorName="Boby Marleni"
+                    />
+                </Styled.BarPlayer>
+                <PlayerVolume
+                    value={audioRef.current.volume}
+                    onVolumeChange={onVolumeChange}
+                    onVolumeToggle={() => onVolumeToggle()}
+                    onVolumeOn={isVolumeOn}
+                />
+            </Styled.BarPlayerBlock>
+        </Styled.BarPlayerContent>
     );
 };
 
